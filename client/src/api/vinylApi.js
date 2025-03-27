aa19743b06f712d27951c148b34b774b247c67cd
vmmineva <veronika.mineva93@gmail.com>
2025-03-27 17:19:48 +0200

    Add use vinyls api

---
+++ client/src/api/vinylApi.js
@@ -0,0 +1,83 @@
+import useAuth from "../hooks/useAuth";
+import request from "../utils/request";
+import {  useEffect, useState } from "react";
+
+const baseUrl = 'http://localhost:3030/data/vinyls';
+
+export const useVinyls = () => {
+    const [vinyls, setVinyls] = useState([]);
+
+    useEffect(() => {
+        request.get(baseUrl)
+            .then(setVinyls)
+    }, []);
+
+    return {
+        vinyls,
+    }
+};
+
+export const useLatestVinyls = () => {
+    const [latestVinyls, setLatestVinyls] = useState([]);
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams({
+            sortBy: '_createdOn desc',
+            pageSize: 3,
+            select: '_id,imageUrl,title,album,artist',
+        })
+        request.get(`${baseUrl}?${searchParams.toString()}`)
+            .then(setLatestVinyls)
+    }, []);
+
+    return {
+        latestVinyls,
+    };
+};
+
+export const useVinyl = (vinylId) => {
+    const [vinyl, setVinyl] = useState({});
+
+    useEffect(() => {
+        request.get(`${baseUrl}/${vinylId}`)
+            .then(setVinyl);
+    }, [vinylId])
+
+    return {
+        vinyl,
+    };
+};
+
+export const useCreateVinyl = () => {
+    const { request } = useAuth();
+
+    const create = (vinylData) => {
+        request.post(baseUrl, vinylData);
+    }
+
+    return {
+        create,
+    }
+};
+
+export const useEditVinyl = () => {
+    const { request } = useAuth()
+
+    const edit = (vinylId, vinylData) => 
+        request.put(`${baseUrl}/${vinylId}`, { ...vinylData, _id: vinylId });
+
+    return {
+        edit,
+    }
+};
+
+export const useDeleteVinyl = () => {
+    const { request } = useAuth();
+
+    const deleteVinyl = (vinylId) => 
+        request.delete(`${baseUrl}/${vinylId}`);
+
+    return {
+        deleteVinyl,
+    }
+};
