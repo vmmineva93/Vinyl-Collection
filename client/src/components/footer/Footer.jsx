8484b9502f440d3477a44575339d26ada22d210d
vmmineva <veronika.mineva93@gmail.com>
2025-03-27 17:32:29 +0200

    Add Header and Footer components

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router";
+
+export default function Footer() {
+    return (
+        <>
+            <div className="container-fluid bg-dark text-secondary footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
+                <div className="container">
+                    <div className="footer-wrapper">
+                        <div className="col-lg-3 col-md-6">
+                            <h5 className="text-light mb-4">Address</h5>
+                            <p className="mb-2"><i className="fa fa-map-marker-alt me-3"></i>999 Street, Sofia, Bulgaria</p>
+                            <p className="mb-2"><i className="fa fa-phone-alt me-3"></i>+359 99 99 99</p>
+                            <p className="mb-2"><i className="fa fa-envelope me-3"></i>antisocialsocialclub@gmail.com</p>
+                            <div className="d-flex pt-2">
+                                <Link className="btn btn-square btn-outline-secondary rounded-circle me-2" href=""><i className="fab fa-twitter"></i></Link>
+                                <Link className="btn btn-square btn-outline-secondary rounded-circle me-2" href=""><i className="fab fa-facebook-f"></i></Link>
+                                <Link className="btn btn-square btn-outline-secondary rounded-circle me-2" href=""><i className="fab fa-youtube"></i></Link>
+                                <Link className="btn btn-square btn-outline-secondary rounded-circle me-2" href=""><i className="fab fa-linkedin-in"></i></Link>
+                            </div>
+                           
+                        </div>
+                        <img src="../../../public/images/cat2.jpg" height="275px" width="500px" alt="" />
+                    </div>
+                </div>
+                <div className="container-fluid">
+                    <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <Link className="border-bottom" href="/">AntiSocialSocialClub</Link>, All Right Reserved.
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router";
+import useAuth from "../../hooks/useAuth";
+
+export default function Header() {
+   const { email, isAuthenticated } = useAuth();
+
+   return (
+      <nav className="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 px-4 px-lg-5">
+         <Link to="/" className="navbar-brand d-flex align-items-center">
+            <img src="../../../public/images/logoClub.jpg" width='50' alt="logo" />
+         </Link>
+         <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span className="navbar-toggler-icon"></span>
+         </button>
+         <div className="collapse navbar-collapse" id="navbarCollapse">
+            <div className="navbar-nav ms-auto py-4 py-lg-0">
+               <Link to="/" className="nav-item nav-link">Home</Link>
+               <Link to="/vinyls" className="nav-item nav-link">All vinyls</Link>
+               {isAuthenticated ? (
+                  <>
+                     <Link to="/vinyls/create" className="nav-item nav-link">Create</Link>
+                     <Link to="/logout" className="nav-item nav-link">Logout</Link> 
+                  </>)
+                  : (<><Link to="/login" className="nav-item nav-link">Login</Link>
+                     <Link to="/register" className="nav-item nav-link">Register</Link> </>)}
+            </div>
+            <div className="h-100 d-lg-inline-flex align-items-center d-none">
+               <Link className="btn btn-square rounded-circle bg-light text-primary me-2" to=""><i className="fab fa-facebook-f"></i></Link>
+               <Link className="btn btn-square rounded-circle bg-light text-primary me-2" to=""><i className="fab fa-twitter"></i></Link>
+               <Link className="btn btn-square rounded-circle bg-light text-primary me-2" to=""><i className="fab fa-linkedin-in"></i></Link>
+               <Link className="btn btn-square rounded-circle bg-light text-primary me-0" to=""><i className="fab fa-instagram"></i></Link>
+            </div>
+         </div>
+      </nav>
+   );
+}
