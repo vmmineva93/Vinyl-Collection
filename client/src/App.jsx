ea6cbafd208c8bf2267058aeceb82615973a6ce0
vmmineva <veronika.mineva93@gmail.com>
2025-03-30 13:49:28 +0300

    Add auth and guest guard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,45 +8,37 @@ import Login from "./components/login/Login"
 import Register from "./components/register/Register"
 import Create from "./components/create/Create"
 import Details from "./components/details/Details"
-import { UserContext } from "./context/UserContext"
 import Edit from "./components/edit/Edit"
 import Logout from "./components/logout/Logout"
-import usePersistedState from "./hooks/usePersistedState"
-
+import UserProvider from "./providers/UserProvider"
+import AuthGuard from "./guards/AuthGuard"
+import GuestGuard from "./guards/GuestGuard"
 
 function App() {
 
-  const [authData, setAuthData] = usePersistedState('auth', {});
-
-  const userLoginHandler = (resultData) => {
-    setAuthData(resultData)
-
-  };
-
-  const userLogoutHandler = () => {
-    setAuthData({});
-  }
-
-
   return (
-    <UserContext.Provider value={{ ...authData, userLoginHandler, userLogoutHandler }}>
-    <div id="box">
-      <Header />
-      <main id="main-content">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/vinyls" element={<Catalog />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/vinyls/create" element={<Create />}></Route>
-          <Route path="/vinyls/:vinylId/details" element={<Details />}></Route>
-          <Route path="/vinyls/:vinylId/edit" element={<Edit />}></Route>
-          <Route path="/logout" element={<Logout />}></Route>
-        </Routes>
-      </main>
-      < Footer />
-    </div>
-    </UserContext.Provider>
+    <UserProvider>
+      <div id="box">
+        <Header />
+        <main id="main-content">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/vinyls" element={<Catalog />}></Route>
+            <Route path="/vinyls/:vinylId/details" element={<Details />}></Route>
+            <Route element={<AuthGuard />}>
+              <Route path="/vinyls/create" element={<Create />}></Route>
+              <Route path="/vinyls/:vinylId/edit" element={<Edit />}></Route>
+              <Route path="/logout" element={<Logout />}></Route>
+            </Route>
+            <Route element={<GuestGuard />}>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/register" element={<Register />}></Route>
+            </Route>
+          </Routes>
+        </main>
+        < Footer />
+      </div>
+    </UserProvider>
   )
 }
 
